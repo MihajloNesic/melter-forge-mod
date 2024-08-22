63986f30885d8013ef5bbe2cf42fb51319c4f535
Oier Bravo Urtasun <oierbravo@gmail.com>
2024-08-22 08:41:18 +0200

    WIP datapack from kube

--- src/main/java/com/oierbravo/melter/compat/kubejs/MelterPlugin.java
+++ src/main/java/com/oierbravo/melter/compat/kubejs/MelterPlugin.java
@@ -6,9 +6,31 @@ import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchemaRegistry;
 
 public class MelterPlugin implements KubeJSPlugin {
+    //public static EventGroup GROUP = EventGroup.of("MelterEvents");
+    //public static EventHandler HEAT_SOURCE = GROUP.startup("registerHeatSource", () -> HeatSourceEvent.class);
+    //public static EventHandler HEAT_SOURCE_SERVER = GROUP.server("registerHeatSource", () -> HeatSourceEvent.class);
 
     @Override
     public void registerRecipeSchemas(RecipeSchemaRegistry registry) {
         registry.namespace(Melter.MODID).register(MeltingRecipe.ID, MeltingRecipeSchema.SCHEMA);
     }
+
+    /*@Override
+    public void registerServerRegistries(ServerRegistryRegistry registry) {
+        registry.register(HeatSourcesRegistry.HEAT_SOURCE_REGISTRY_KEY, HeatSource.CODEC, HeatSource.class);
+    }
+
+    @Override
+    public void registerBuilderTypes(BuilderTypeRegistry registry) {
+        registry.addDefault(HeatSourcesRegistry.HEAT_SOURCE_REGISTRY_KEY, HeatSourceBuilderKube.class, HeatSourceBuilderKube::new);
+    }
+
+    @Override
+    public void registerEvents(EventGroupRegistry registry) {
+        registry.register(GROUP);
+    }*/
+
+
+
 }
+
